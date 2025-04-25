d38d70c8847993cedff2dd9dd727cfc9af9805d4
Eduard Späck <edi.spack@gmail.com>
2025-04-25 23:37:04 +0300

    add test controller

---
+++ microservices/gateway/src/main/kotlin/com/democloudproject/gateway/TestController.kt
@@ -0,0 +1,13 @@
+package com.democloudproject.gateway
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+class TestController {
+    @GetMapping("/test")
+    fun helloWorld(): String {
+        return "Hello, World!"
+    }
+}
