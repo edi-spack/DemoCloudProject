2e52f988f3af6a9566c72c6941117f7cd8a911d5
Eduard Späck <edi.spack@gmail.com>
2025-04-26 09:53:02 +0300

    test/fix

--- microservices/gateway/src/main/kotlin/com/democloudproject/gateway/TestController.kt
+++ microservices/gateway/src/main/kotlin/com/democloudproject/gateway/TestController.kt
@@ -10,4 +10,9 @@ class TestController {
     fun helloWorld(): String {
         return "Hello, World!"
     }
+
+    @GetMapping("/")
+    fun healthCheck(): String {
+        return "OK"
+    }
 }
